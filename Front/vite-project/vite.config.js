b530ed30b2d6a559701f1e7e56509907804d9026
egortg <egorgorodinskij85@gmail.com>
2026-06-17 16:32:40 +0300

    fix 24

--- Front/vite-project/vite.config.js
+++ Front/vite-project/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
@@ -8,21 +7,5 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        assetFileNames: 'assets/[name]-[hash][extname]',
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-      }
-    }
-  },
-  server: {
-    port: 5173,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@images': path.resolve(__dirname, './images'),
-    }
   }
 })
