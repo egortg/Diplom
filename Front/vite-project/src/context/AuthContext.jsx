0da818fe1b5cbdc011d1b9f57e395ad6be6e7149
egortg <egorgorodinskij85@gmail.com>
2026-06-17 14:43:04 +0300

    fix 8

--- Front/vite-project/src/context/AuthContext.jsx
+++ Front/vite-project/src/context/AuthContext.jsx
@@ -6,7 +6,7 @@ const AuthContext = createContext();
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
 
 export const useAuth = () => useContext(AuthContext);
-
+ 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
